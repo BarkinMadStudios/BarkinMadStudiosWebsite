23e82426257727e1922929237284c5ff33cccd3f
Barry King <84771273+barkin-cyb@users.noreply.github.com>
2026-06-26 11:50:18 +0100

    Use Service Worker syntax for Cloudflare versions deploy pipeline compatibility

--- worker.js
+++ worker.js
@@ -1,8 +1,6 @@
-export default {
-  async fetch(request, env, ctx) {
-    return handleRequest(request);
-  }
-};
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
 
 const ADS_TXT = `google.com, pub-2030137443667873, DIRECT, f08c47fec0942fa0
 `;
